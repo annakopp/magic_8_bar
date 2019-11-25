5dd43f591f155f688018479f311acd673fe3ccc8
Anna Kopp <koppanna@gmail.com>
2019-11-25 18:41:33 -0500

    Initial Commit

---
+++ tab_bar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tab_bar
+//
+//  Created by Anna on 11/25/19.
+//  Copyright © 2019 Anna. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ tab_barTests/tab_barTests.swift
@@ -0,0 +1,34 @@
+//
+//  tab_barTests.swift
+//  tab_barTests
+//
+//  Created by Anna on 11/25/19.
+//  Copyright © 2019 Anna. All rights reserved.
+//
+
+import XCTest
+@testable import tab_bar
+
+class tab_barTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
